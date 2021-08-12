2c4df38582798065c845d6fc1a1583644dcbc50f
Ilia Romanov <iliaromanov033@gmail.com>
2021-08-11 23:03:53 -0400

    Update palindrome_number.py

--- palindrome_number.py
+++ palindrome_number.py
@@ -1,6 +1,7 @@
 # Given an integer x, return true if x is palindrome integer.
 # An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
 
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         return str(x)[::-1] == str(x)
