867c237ea0a5cfd1761fc47a9f93513da6b67361
Iliaromanov <iliaromanov033@gmail.com>
2022-04-27 18:15:48 -0400

    generalized binary search template

---
+++ generalized_binary_search_template.py
@@ -0,0 +1,17 @@
+
+"""
+Task: Minimize k , s.t. condition(k) is True
+"""
+
+def binary_search(array) -> int:
+    def condition(value) -> bool:
+        pass
+
+    left, right = 0, len(array)
+    while left < right:
+        mid = left + (right - left) // 2
+        if condition(mid):
+            right = mid
+        else:
+            left = mid + 1
+    return left
