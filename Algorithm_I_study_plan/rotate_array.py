688ff356c9db845f2893546bd7884693043fef84
Iliaromanov <iliaromanov033@gmail.com>
2022-08-23 18:38:04 -0400

    rotate array smart solution

--- Algorithm_I_study_plan/rotate_array.py
+++ Algorithm_I_study_plan/rotate_array.py
@@ -15,3 +15,37 @@ class Solution:
         
         for i, num in enumerate(result):
             nums[(i + steps) % len(nums)] = num
+
+
+
+class Solution:
+    """
+    O(n) time, O(1) space
+    """
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        steps = k % len(nums)
+        
+        # reverse entire list
+        l, r = 0, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+            
+        # reverse up to k - 1
+        l, r = 0, steps - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+        
+        # reverse k to end of list
+        l, r = steps, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+            
