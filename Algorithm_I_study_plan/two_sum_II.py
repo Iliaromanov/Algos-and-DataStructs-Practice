eeb24a1ad1a46b662327eedd3b0c883d48d1afaa
Iliaromanov <iliaromanov033@gmail.com>
2022-08-24 11:04:21 -0400

    two sum II binary search solution

---
+++ Algorithm_I_study_plan/two_sum_II.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+        
+        while l < r:
+            m = l + (r - l) // 2
+            result = numbers[l] + numbers[r]
+            if result == target:
+                return [l + 1, r + 1]
+            elif result < target:
+                if numbers[m] + numbers[r] < target:
+                    l = m + 1
+                else:
+                    l += 1
+            else:
+                if numbers[m] + numbers[l] > target:
+                    r = m - 1
+                else:
+                    r -= 1
+        
+        return -1
+            
