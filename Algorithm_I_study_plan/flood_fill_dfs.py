5c2b398b1c4d291e21b4a992044925556b0fd178
Iliaromanov <iliaromanov033@gmail.com>
2022-08-28 11:54:01 -0400

    flood fill new cleaner solution

--- Algorithm_I_study_plan/flood_fill_dfs.py
+++ Algorithm_I_study_plan/flood_fill_dfs.py
@@ -1,6 +1,42 @@
 # Flood Fill solution using DFS
 from typing import List, Tuple
 
+"""New Cleaner Solution"""
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        
+        orig_color = image[sr][sc]
+        if orig_color == color:
+            return image
+        
+        def dfs(row, col):
+            image[row][col] = color
+            
+            print(row, col)
+            
+            # above
+            if row > 0 and image[row-1][col] == orig_color:
+                dfs(row-1, col)
+                
+            # below
+            if row < len(image) - 1 and image[row+1][col] == orig_color:
+                dfs(row+1, col)
+            
+            # left
+            if col > 0 and image[row][col-1] == orig_color:
+                dfs(row, col-1)
+            
+            # right
+            if col < len(image[0]) - 1 and image[row][col+1] == orig_color:
+                dfs(row, col+1)
+                
+        dfs(sr, sc)
+        
+        return image
+
+
+
+"""Old Solution"""
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, new_color: int) -> List[List[int]]:
         visited = []
