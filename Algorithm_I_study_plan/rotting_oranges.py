820f5bfeddb758880220d3c6937cb30f94d9f176
Ilia Romanov <iliaromanov033@gmail.com>
2022-09-24 19:08:21 -0400

    rotting oranges cleaner solution

--- Algorithm_I_study_plan/rotting_oranges.py
+++ Algorithm_I_study_plan/rotting_oranges.py
@@ -43,3 +43,49 @@ class Solution:
                 return steps_count
             
         return -1
+
+
+# slightly cleaner solution
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        minutes = 0
+        to_rot_count = 0
+        queue = []
+        visited = set()
+        
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 2:
+                    queue.append((r, c))
+                    visited.add((r, c))
+                elif grid[r][c] == 1:
+                    to_rot_count += 1
+        
+        # bfs
+        l = len(queue)
+        while queue:
+            
+            if to_rot_count == 0:
+                return minutes
+            
+            minutes += 1
+            l = len(queue)
+            while l:
+                r, c = queue.pop(0)
+
+                for x, y in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+                    new_r = r + y
+                    new_c = c + x
+
+                    if 0 <= new_r < len(grid) and \
+                       0 <= new_c < len(grid[0]) and (new_r, new_c) not in visited \
+                       and grid[new_r][new_c] == 1:
+                        grid[new_r][new_c] = 2
+                        queue.append((new_r, new_c))
+                        to_rot_count -= 1
+                        visited.add((new_r, new_c))
+                l -= 1
+        
+        if to_rot_count:
+            return -1
+        return minutes
