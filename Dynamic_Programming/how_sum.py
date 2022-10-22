2f111b0e856926fea82587cdb5a4088754613ab7
Ilia Romanov <iliaromanov033@gmail.com>
2022-10-22 11:51:53 -0400

    how_sum memoization solution

--- Dynamic_Programming/how_sum.py
+++ Dynamic_Programming/how_sum.py
@@ -15,6 +15,7 @@ def how_sum_recursive(target: int, nums: List[int]) -> List[List[int]]:
             cur.append(num)
 
             result = backtrack(t - num, cur[::])
+
             if result:
                 return result 
 
@@ -25,4 +26,36 @@ def how_sum_recursive(target: int, nums: List[int]) -> List[List[int]]:
     return backtrack(target, [])
 
 
-print(how_sum_recursive(7, [5, 3, 4, 7]))
+def how_sum_memo(target: int, nums: List[int]) -> List[List[int]]:
+    """
+    Returns ANY SINGLE combinations from nums that sums to target
+    """
+
+    def backtrack_memo(t: int, cur: List[int], memo = {}):
+        if t == 0:
+            return cur
+        if t < 0:
+            return None
+
+        if t in memo:
+            return memo[t]
+
+        for num in nums:
+            cur.append(num)
+
+            result = backtrack_memo(t - num, cur[::])
+            memo[t] = result
+            if result:
+                return result 
+
+            cur.pop()
+
+        memo[t] = None
+        return None
+
+    return backtrack_memo(target, [])
+
+
+print(how_sum_recursive(8, [2, 3, 5]))
+print(how_sum_memo(300, [7, 14]))
+print(how_sum_memo(7, [5, 3, 4, 7]))
