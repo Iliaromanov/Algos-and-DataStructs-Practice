88193a33419c8268702e5a773a4d97fffb3a4c14
Ilia Romanov <iliaromanov033@gmail.com>
2022-11-02 00:06:01 -0400

    best sum recursion improved

--- Dynamic_Programming/best_sum.py
+++ Dynamic_Programming/best_sum.py
@@ -6,28 +6,25 @@ def best_sum(target: int, nums: List[int]) -> int:
     Returns the smallest subset of nums that sums to target.
     None if no such subset exists
     """
-    result = None
-
-    def backtrack(target: int, subset: List[int]):
-        nonlocal result
-
-        if target == 0 and (not result or len(subset) < len(result)):
-            result = subset[::]
-            return
-        if target < 0:
-            return None
-
-        for num in nums:
-            subset.append(num)
-
-            backtrack(target - num, subset[::])
-
-            subset.pop()
-
+    if target == 0:
+        return []
+    
+    if target < 0:
         return None
 
-    backtrack(target, [])
+    shortest_combo = None
 
-    return result
+    for num in nums:
+        remainder = target - num
+        remainder_comb = best_sum(remainder, nums)
+        if remainder_comb != None:
+            comb = remainder_comb + [num]
+            if not shortest_combo or len(comb)<len(shortest_combo):
+                shortest_combo = comb
+    
+    return shortest_combo
 
-print(best_sum(8, [2, 3, 5]))
+
+print(best_sum(8, [1, 5, 4]))
+print(best_sum(100, [1, 2, 5, 25]))
+
