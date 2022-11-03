c48ec8ceb1b9d840c4ff23029b0393baf209ad4e
Ilia Romanov <iliaromanov033@gmail.com>
2022-11-02 23:06:20 -0400

    best sum memoization

--- Dynamic_Programming/best_sum.py
+++ Dynamic_Programming/best_sum.py
@@ -24,7 +24,29 @@ def best_sum(target: int, nums: List[int]) -> int:
     
     return shortest_combo
 
+def best_sum_memo(target: int, nums: List[int], memo={}) -> int:
+    """
+    Returns the smallest subset of nums that sums to target.
+    None if no such subset exists
+    """
+    if target in memo:
+        return memo[target]
 
-print(best_sum(8, [1, 5, 4]))
-print(best_sum(100, [1, 2, 5, 25]))
+    if target == 0:
+        return []
+    
+    if target < 0:
+        return None
 
+    shortest_combo = None
+
+    for num in nums:
+        remainder = target - num
+        remainder_combo = best_sum_memo(remainder, nums, memo)
+        if remainder_combo != None:
+            comb = remainder_combo + [num]
+            if not shortest_combo or len(comb)<len(shortest_combo):
+                shortest_combo = comb
+    
+    memo[target] = shortest_combo
+    return shortest_combo
