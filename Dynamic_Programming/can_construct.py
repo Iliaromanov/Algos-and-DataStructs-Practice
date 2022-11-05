aa64515814bec60ff4abef8739b5bcb3517ee320
Ilia Romanov <iliaromanov033@gmail.com>
2022-11-04 23:13:05 -0400

    can_construct remove unnecessary case

--- Dynamic_Programming/can_construct.py
+++ Dynamic_Programming/can_construct.py
@@ -5,20 +5,12 @@ def can_construct(target_word: str, word_bank: List[str]) -> bool:
     Returns true if target_word can be constructed by concatenating
     strings from the word_bank
     """
-    print(target_word)
     if target_word == "":
         return True
 
-
     for s in word_bank:
         if target_word.startswith(s):
-            print("starts with ", s)
             if can_construct(target_word[len(s):], word_bank):
-                return True
-        
-        if target_word.endswith(s):
-            print("ends with ", s)
-            if can_construct(target_word[:len(target_word) - len(s)], word_bank):
                 return True
 
     return False           
@@ -28,3 +20,4 @@ def can_construct(target_word: str, word_bank: List[str]) -> bool:
 
 
 print(can_construct("abcdef", ['ab', 'abc', 'cd', 'def', 'abcd']))
+print(can_construct("skateboard", ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']))
