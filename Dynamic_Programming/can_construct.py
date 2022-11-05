ee925136f118f94a906477ed6b863671c76bd4cf
Ilia Romanov <iliaromanov033@gmail.com>
2022-11-04 23:39:54 -0400

    can construct memoization

--- Dynamic_Programming/can_construct.py
+++ Dynamic_Programming/can_construct.py
@@ -13,11 +13,33 @@ def can_construct(target_word: str, word_bank: List[str]) -> bool:
             if can_construct(target_word[len(s):], word_bank):
                 return True
 
-    return False           
+    return False
+
+
+def can_construct_memo(target_word: str, word_bank: List[str], memo = {}) -> bool:
+    """
+    Returns true if target_word can be constructed by concatenating
+    strings from the word_bank
+    """
+    if target_word in memo:
+        return memo[target_word]
+    
+    if target_word == "":
+        return True
+
+    for s in word_bank:
+        if target_word.startswith(s):
+            memo[target_word] = can_construct_memo(target_word[len(s):], word_bank, memo)
+            if memo[target_word]:
+                return True
+
+    memo[target_word] = False
+    return False      
 
 
 
 
 
-print(can_construct("abcdef", ['ab', 'abc', 'cd', 'def', 'abcd']))
-print(can_construct("skateboard", ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']))
+print(can_construct_memo("abcdef", ['ab', 'abc', 'cd', 'def', 'abcd']))
+print(can_construct_memo("skateboard", ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']))
+print(can_construct_memo("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeeee", "eeeeeeee"]))
