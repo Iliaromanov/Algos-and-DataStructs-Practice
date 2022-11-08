977799fe5c4bfe255b12c5a53c59b7693e16bc95
Ilia Romanov <iliaromanov033@gmail.com>
2022-11-07 23:14:15 -0500

    all construct memoization

--- Dynamic_Programming/all_construct.py
+++ Dynamic_Programming/all_construct.py
@@ -18,8 +18,28 @@ def all_construct(target: str, words: List[str]) -> List[List[str]]:
     return combs
 
 
+def all_construct_memo(target: str, words: List[str], memo={}) -> List[List[str]]:
+    """
+    Returns all the ways target can be constructed by concatenating
+    the words in words
+    """
+    if target in memo:
+        return memo[target]
 
+    if target == "":
+        return [[]]
 
-print(all_construct("purple", ['purp', 'p', 'ur', 'le', 'purpl']))
-print(all_construct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd', 'ef', 'c']))
-print(all_construct('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaz', ['a', 'aa', 'aaa', 'aaaa', 'aaaaa']))
+    combs = []
+    for word in words:
+        if target.startswith(word):
+            comb = all_construct_memo(target[len(word):], words, memo)
+            if comb:
+                comb = [c + [word] for c in comb]
+                combs.extend(comb)
+
+    memo[target] = combs
+    return combs
+
+print(all_construct_memo("purple", ['purp', 'p', 'ur', 'le', 'purpl']))
+print(all_construct_memo('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd', 'ef', 'c']))
+print(all_construct_memo('aaaaaaaaaaaaaaaaaaaaaaaaaaaaa', ['a', 'aa', 'aaa', 'aaaa', 'aaaaa']))
