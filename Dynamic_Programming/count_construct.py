f28102ac7873bd30e2a17edef3fd0d9f9729c74f
Ilia Romanov <iliaromanov033@gmail.com>
2022-11-07 21:21:26 -0500

    count construct memoization

--- Dynamic_Programming/count_construct.py
+++ Dynamic_Programming/count_construct.py
@@ -17,5 +17,27 @@ def count_construct(target: str, words: List[str]) -> int:
     return count
 
 
-print(count_construct("abcdef", ['ab', 'abc', 'cd', 'def', 'abcd']))
-print(count_construct("purple", ['purp', 'p', 'ur', 'le', 'purpl'])) 
+def count_construct_memo(target: str, words: List[str], memo={}) -> int:
+    """
+    Returns the number of ways target can be constructed by
+    concatenating strings from words
+    """
+    if target in memo:
+        return memo[target]
+    
+    if target == "":
+        return 1
+
+    count = 0
+    for word in words:
+        if target.startswith(word):
+            count += count_construct_memo(target[len(word):], words, memo)
+
+    memo[target] = count
+    return count
+
+
+
+print(count_construct_memo("abcdef", ['ab', 'abc', 'cd', 'def', 'abcd']))
+print(count_construct_memo("purple", ['purp', 'p', 'ur', 'le', 'purpl']))
+print(count_construct_memo("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeeee", "eeeeeeee"]))
