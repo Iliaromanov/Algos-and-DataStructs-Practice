4db76241e8898ada3260195b7b1ccde28b5f8ae7
Ilia Romanov <iliaromanov033@gmail.com>
2022-11-28 22:39:29 -0500

    how sum tabulation

--- Dynamic_Programming/how_sum.py
+++ Dynamic_Programming/how_sum.py
@@ -56,6 +56,31 @@ def how_sum_memo(target: int, nums: List[int]) -> List[List[int]]:
     return backtrack_memo(target, [])
 
 
+def how_sum_tab(target: int, nums: List[int]):
+    table = [[] for _ in range(target + 1)]
+    table[0].append([])
+
+    for i, how_sum_to_i in enumerate(table):
+        if len(how_sum_to_i) > 0:
+            for num in nums:
+                if i+num < target + 1:
+                    # need just one combination
+                    table[i+num].append(how_sum_to_i[0] + [num])
+                
+                if i + num == target:
+                    break
+
+    if table[target]:
+        return table[target][0]
+    return None
+
+
 print(how_sum_recursive(8, [2, 3, 5]))
 print(how_sum_memo(300, [7, 14]))
 print(how_sum_memo(7, [5, 3, 4, 7]))
+
+print("======")
+
+print(how_sum_tab(8, [2, 3, 5]))
+print(how_sum_tab(300, [7, 14]))
+print(how_sum_tab(7, [5, 3, 4, 7]))
