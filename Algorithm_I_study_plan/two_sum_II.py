7e161d758e30fff77851c8d1a2bb491c419e2831
Ilia Romanov <iliaromanov033@gmail.com>
2023-01-02 12:45:49 -0500

    alternate solution for two sum II

--- Algorithm_I_study_plan/two_sum_II.py
+++ Algorithm_I_study_plan/two_sum_II.py
@@ -21,4 +21,17 @@ class Solution:
                     r -= 1
         
         return -1
+
+
+class SolutionAlt:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+
+        while 1:
+            if numbers[l] + numbers[r] > target:
+                r -= 1
+            elif numbers[l] + numbers[r] < target:
+                l += 1
+            else:
+                return [l+1, r+1]
             
