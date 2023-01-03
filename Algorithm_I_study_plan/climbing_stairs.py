bc1ef8fd72ec117ebd75ffe9812c4783c4158d85
Ilia Romanov <iliaromanov033@gmail.com>
2023-01-03 14:36:32 -0500

    climbing stairs tabulation solution

--- Algorithm_I_study_plan/climbing_stairs.py
+++ Algorithm_I_study_plan/climbing_stairs.py
@@ -16,4 +16,20 @@ class Solution:
             return solved[n]
         
         return memoization(n)
+
+
+"""TABULATION"""
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        table = [0 for _ in range(n+1)]
+        table[1] = 1
+        table[2] = 1
+
+        for i, num in enumerate(table):
+            if i+1 < len(table): table[i+1] += num
+            if i+2 < len(table): table[i+2] += num
+
+        return table[n]
         
