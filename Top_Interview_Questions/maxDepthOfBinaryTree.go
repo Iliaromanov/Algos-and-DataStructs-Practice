29c7d311b6a2aa4770298e3c3c6130863f94e7b9
Iliaromanov <iliaromanov033@gmail.com>
2023-05-26 11:19:36 -0400

    max depth of binary tree go solution

---
+++ Top_Interview_Questions/maxDepthOfBinaryTree.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ func maxDepth(root *TreeNode) int {
+    if root == nil {
+        return 0;
+    }
+    return max(maxDepth(root.Left) + 1, maxDepth(root.Right) + 1);
+}
+
+func max(a int, b int) int {
+    if a > b {
+        return a;
+    }
+    return b;
+}
