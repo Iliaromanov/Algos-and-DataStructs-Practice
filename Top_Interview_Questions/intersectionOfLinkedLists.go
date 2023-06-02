07ee02935227c52f2fa64832243a72bc736d05e3
Iliaromanov <iliaromanov033@gmail.com>
2023-06-02 19:59:22 -0400

    intersection of two linked lists optimal two pointer solution

--- Top_Interview_Questions/intersectionOfLinkedLists.go
+++ Top_Interview_Questions/intersectionOfLinkedLists.go
@@ -6,25 +6,42 @@
  * }
  */
 
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	a, b := headA, headB
 
+	for a != b {
+		if a == nil {
+			a = headB
+		} else {
+			a = a.Next
+		}
 
+		if b == nil {
+			b = headA
+		} else {
+			b = b.Next
+		}
+	}
 
- // map solution
+	return a
+}
+
+// map solution
 func getIntersectionNodeMap(headA, headB *ListNode) *ListNode {
-    m := make(map[*ListNode]int);
+	m := make(map[*ListNode]int)
 
-    a, b := headA, headB;
+	a, b := headA, headB
 
-    for a != nil {
-        m[a] = 1;
-        a = a.Next;
-    }
+	for a != nil {
+		m[a] = 1
+		a = a.Next
+	}
 
-    for b != nil {
-        if _, found := m[b]; found {
-            return b;
-        }
-        b = b.Next;
-    }
-    return nil;
+	for b != nil {
+		if _, found := m[b]; found {
+			return b
+		}
+		b = b.Next
+	}
+	return nil
 }
