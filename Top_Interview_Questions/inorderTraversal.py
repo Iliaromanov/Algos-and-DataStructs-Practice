2d7007c46c1dcf19c0b48f0c9444c31e413f0a92
Iliaromanov <iliaromanov033@gmail.com>
2023-07-04 16:37:31 -0400

    inorder traversal iterative version python

--- Top_Interview_Questions/inorderTraversal.py
+++ Top_Interview_Questions/inorderTraversal.py
@@ -2,3 +2,25 @@ class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         if root is None: return []
         return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
+    
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        result = []
+        q = [root]
+
+        while q:
+            top = q[-1]
+            if top.left is not None:
+                q.append(top.left)
+                top.left = None 
+                continue
+            
+            result.append(q.pop().val)
+
+            if top.right is not None:
+                q.append(top.right)
+
+        return result
