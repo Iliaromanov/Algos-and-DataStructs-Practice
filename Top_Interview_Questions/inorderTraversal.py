01a90c4f0e4bf7053d2ea22ac001435963d098b4
Iliaromanov <iliaromanov033@gmail.com>
2023-07-04 15:01:40 -0400

    inorder traversal simple python solution

---
+++ Top_Interview_Questions/inorderTraversal.py
@@ -0,0 +1,4 @@
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None: return []
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
