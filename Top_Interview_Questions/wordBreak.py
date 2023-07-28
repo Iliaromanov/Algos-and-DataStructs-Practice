0eea2f8541945c00db2b8b1db7403eddb6cfeed2
Iliaromanov <iliaromanov033@gmail.com>
2023-07-28 12:08:47 -0400

    word break O(n * m * k) solution

--- Top_Interview_Questions/wordBreak.py
+++ Top_Interview_Questions/wordBreak.py
@@ -1,3 +1,44 @@
+
+
+"""
+
+let n = len(s), m = len(wordDict), k = max(len(word) for word in wordDict)
+
+"""
+
+
+
+"""
+O(n * m * k)
+"""
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        """
+        subprob: dp(i) = can parse s[i:]
+        answer:  dp(0)
+        recurrence: dp(i) = s[i] in wordDict and dp(j, j+1) 
+                            or
+                               dp(i, j+1)
+        base cases: if j == len(s): dp(i, j) = s[i:j] in wordDict
+                    dp(len(s), len(s)) = True
+        """
+        dp = [False for _ in range(len(s)+1)]
+
+        dp[len(s)] = True
+
+        for i in range(len(s)-1, -1, -1): # O(len(s))
+            for word in wordDict:
+                if i+len(word) <= len(s) and s[i:i+len(word)] == word: # O(len(word))
+                    dp[i] = dp[i+len(word)]
+                    if dp[i] is True: break
+        return dp[0]
+
+
+
+
+"""
+O(n^2)
+"""
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         """
