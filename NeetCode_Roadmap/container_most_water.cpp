1f4e7619c2b97c70b2e4a11f6b3184aaa6c0542b
Iliaromanov <iliaromanov033@gmail.com>
2023-11-19 13:47:22 +0000

    container with most water

---
+++ NeetCode_Roadmap/container_most_water.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0; int r = height.size() - 1;
+        int best = 0;
+        while (l < r) {
+            best = max(best, (r-l) * min(height[l], height[r]));
+            if (height[l] > height[r]) --r;
+            else ++l;
+        }
+        return best;
+    }
+};
