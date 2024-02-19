6664378d70610573257a9e4285afb104430f5eed
Iliaromanov <iliaromanov033@gmail.com>
2024-02-19 16:36:19 -0500

    time based key value store

---
+++ NeetCode_Roadmap/time_based_key_value_store.cpp
@@ -0,0 +1,45 @@
+class TimeMap {
+public:
+    std::unordered_map<std::string, std::vector<std::pair<int, std::string>>> 
+            m_timestamps_map;
+    TimeMap() {}
+    
+    void set(string key, string value, int timestamp) {
+        if (const auto it = m_timestamps_map.find(key); it != m_timestamps_map.end())
+            it->second.emplace_back(timestamp, value);
+        else
+            m_timestamps_map[key] = {std::make_pair(timestamp, value)};
+    }
+    
+    string get(string key, int timestamp) {
+        const auto it = m_timestamps_map.find(key);
+        if (it == m_timestamps_map.end() || it->second.empty() || it->second[0].first > timestamp)
+            return "";
+        
+        // do bsearch
+        int l = 0, r = it->second.size() - 1;
+        while (l <= r)
+        {
+            int m = l + (r - l) / 2;
+
+            if (it->second[m].first == timestamp)
+                return it->second[m].second;
+            else if (it->second[m].first < timestamp)
+                l = m + 1;
+            else // it->second[m].first > timestamp
+                r = m - 1;
+        }
+
+        if (l >= it->second.size())
+            return it->second.back().second;
+
+        return it->second[l-1].second;
+    }
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
