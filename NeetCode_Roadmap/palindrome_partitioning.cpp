0d40c17af0301d3ba198b7866dc3bec97cb53450
Iliaromanov <iliaromanov033@gmail.com>
2024-06-16 21:24:12 -0400

    palindrome partitioning

---
+++ NeetCode_Roadmap/palindrome_partitioning.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> result;
+        vector<string> empty{};
+        backtrack(0, empty, result, s);
+        return result;
+    }
+
+    void backtrack(int start, vector<string>& cur_comb, vector<vector<string>>& result, string_view s)
+    {
+        if (start == s.length())
+        {
+            result.emplace_back(cur_comb);
+            return;
+        }
+
+        for (int i = start+1; i <= s.length(); ++i)
+        {
+            string sub = string(s.substr(start, i-start));
+            if (is_palindrome(sub))
+            {
+                cur_comb.push_back(sub);
+                backtrack(i, cur_comb, result, s);
+                cur_comb.pop_back();
+            }
+        }
+    }
+
+    bool is_palindrome(string_view s)
+    {
+        int l = 0, r = s.length() - 1;
+        while (l < r)
+        {
+            if (s[l] != s[r]) return false;
+            ++l;
+            --r;
+        }
+        return true;
+    }
+};
