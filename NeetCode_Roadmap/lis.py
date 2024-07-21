4fc37bad52c6f983d3e9f1ce6e3e9c4e27a18665
Iliaromanov <iliaromanov033@gmail.com>
2024-07-21 17:07:27 -0400

    lis binary search

---
+++ NeetCode_Roadmap/lis.py
@@ -0,0 +1,12 @@
+import bisect
+
+class Solution:  # 68 ms, faster than 93.92%
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        sub = []
+        for x in nums:
+            if len(sub) == 0 or sub[-1] < x:
+                sub.append(x)
+            else:
+                idx = bisect_left(sub, x)  # Find the index of the first element >= x
+                sub[idx] = x  # Replace that number with x
+        return len(sub)
